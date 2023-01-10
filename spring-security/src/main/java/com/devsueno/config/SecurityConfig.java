f0cc32e9fb18c192e2712964f0f5f80a2ccb8132
Ensueno22 <seongkwon87@gmail.com>
2023-01-10 16:35:38 +0900

    인증 인가 예외 처리에 대한 핸들러 구현.

--- spring-security/src/main/java/com/devsueno/config/SecurityConfig.java
+++ spring-security/src/main/java/com/devsueno/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.devsueno.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.Authentication;
@@ -11,11 +12,16 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -36,7 +42,8 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests((authz) ->
-                        authz.antMatchers("/user").hasRole("USER")
+                        authz.antMatchers("/login").permitAll()
+                                .antMatchers("/user").hasRole("USER")
                                 .antMatchers("/admin/pay").hasRole("ADMIN")
                                 .antMatchers("/admin/**").access("hasRole('ADMIN') or hasRole('SYS')")
                                 .anyRequest().authenticated()
@@ -51,8 +58,10 @@ public class SecurityConfig {
                 .successHandler(new AuthenticationSuccessHandler() {
                     @Override
                     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-                        System.out.println("authentication" + authentication.getName());
-                        response.sendRedirect("/");
+                        RequestCache requestCache = new HttpSessionRequestCache();
+                        SavedRequest savedRequest = requestCache.getRequest(request, response);
+                        String redirectUrl = savedRequest.getRedirectUrl();
+                        response.sendRedirect(redirectUrl);
                     }
                 })
                 .failureHandler(new AuthenticationFailureHandler() {
@@ -83,15 +92,29 @@ public class SecurityConfig {
                 .deleteCookies("remember-me")
                 .and()
                 .sessionManagement()
-                .sessionFixation().changeSessionId(); // 세션 고정 보호 기능
+                .sessionFixation().changeSessionId() // 세션 고정 보호 기능
 //                .maximumSessions(1)
 //                .maxSessionsPreventsLogin(false) // true 인증 초과 시 다음 유저는 접속 안됨; false 는 기존 접근된 세션을 종료 시킴
-        http
+                .and()
                 .rememberMe()
                 .rememberMeParameter("remember")
                 .tokenValiditySeconds(3600)
                 .alwaysRemember(false)
                 .userDetailsService(userDetailsService)
+                .and()
+                .exceptionHandling()
+//                .authenticationEntryPoint(new AuthenticationEntryPoint() {
+//                    @Override
+//                    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+//                        response.sendRedirect("/login"); // 직접 만든 로그인 페이지로 이동된다.
+//                    }
+//                })
+                .accessDeniedHandler(new AccessDeniedHandler() {
+                    @Override
+                    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+                        response.sendRedirect("/denied");
+                    }
+                })
                 .and()
                 .httpBasic(withDefaults());
         return http.build();
