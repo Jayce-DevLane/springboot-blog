f3067bc583694a763e690f4e11933adfdb43ef71
Ensueno22 <seongkwon87@gmail.com>
2023-01-16 19:32:49 +0900

    로그인 & 로그아웃 처리 구현

--- spring-security/src/main/java/com/devsueno/config/security/config/SecurityConfig.java
+++ spring-security/src/main/java/com/devsueno/config/security/config/SecurityConfig.java
@@ -1,11 +1,15 @@
-package com.devsueno.config;
+package com.devsueno.config.security.config;
 
+import com.devsueno.config.security.service.CustomAuthenticationProvider;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
@@ -19,17 +23,30 @@ public class SecurityConfig {
 
         http
                 .authorizeRequests((authz) ->
-                        authz.antMatchers("/", "/user").permitAll()
+                        authz.antMatchers("/", "/users").permitAll()
                                 .antMatchers("/mypage").hasRole("USER")
                                 .antMatchers("/messages").hasRole("MANAGER")
                                 .antMatchers("/config").hasRole("ADMIN")
                                 .anyRequest().authenticated()
                 )
-                .formLogin();
+                .formLogin()
+                .loginPage("/login")
+                .loginProcessingUrl("/login_proc")
+                .defaultSuccessUrl("/")
+                .permitAll();
 
         return http.build();
     }
 
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        return new CustomAuthenticationProvider();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         return (web) -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
---
+++ spring-security/src/main/java/com/devsueno/config/security/service/CustomAuthenticationProvider.java
@@ -0,0 +1,45 @@
+package com.devsueno.config.security.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+
+        String username = authentication.getName();
+        String password = (String)authentication.getCredentials();
+
+        AccountContext context = (AccountContext)userDetailsService.loadUserByUsername(username);
+
+        // 패스워드 인증 실패 시
+        if(passwordEncoder.matches(password, context.getAccount().getPassword())) {
+            throw new BadCredentialsException("BadCredentialsException");
+        }
+
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(context.getAccount(), null, context.getAuthorities());
+
+        return authenticationToken;
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication);
+    }
+}
